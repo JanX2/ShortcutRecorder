d165fd0aea29d72a7442ba30d8dc5a96f039654e
Ilya Kulakov <kulakov.ilya@gmail.com>
2013-01-23 00:59:36 +0700

    Fix SRReadableString* may return SPACE instead of Space.

--- SRCommon.m
+++ SRCommon.m
@@ -19,21 +19,33 @@
 
 NSString *SRReadableStringForCocoaModifierFlagsAndKeyCode(NSUInteger aModifierFlags, unsigned short aKeyCode)
 {
+    SRKeyCodeTransformer *t = [SRKeyCodeTransformer sharedPlainTransformer];
+    NSString *c = [t transformedValue:@(aKeyCode)];
+
+    if (![t isKeyCodeSpecial:aKeyCode])
+        c = [c uppercaseString];
+
     return [NSString stringWithFormat:@"%@%@%@%@%@",
                                       (aModifierFlags & NSCommandKeyMask ? SRLoc(@"Command-") : @""),
                                       (aModifierFlags & NSAlternateKeyMask ? SRLoc(@"Option-") : @""),
                                       (aModifierFlags & NSControlKeyMask ? SRLoc(@"Control-") : @""),
                                       (aModifierFlags & NSShiftKeyMask ? SRLoc(@"Shift-") : @""),
-                                      [[[SRKeyCodeTransformer sharedPlainTransformer] transformedValue:@(aKeyCode)] uppercaseString]];
+                                      c];
 }
 
 
 NSString *SRReadableASCIIStringForCocoaModifierFlagsAndKeyCode(NSUInteger aModifierFlags, unsigned short aKeyCode)
 {
+    SRKeyCodeTransformer *t = [SRKeyCodeTransformer sharedPlainASCIITransformer];
+    NSString *c = [t transformedValue:@(aKeyCode)];
+
+    if (![t isKeyCodeSpecial:aKeyCode])
+        c = [c uppercaseString];
+
     return [NSString stringWithFormat:@"%@%@%@%@%@",
-                                      (aModifierFlags & NSCommandKeyMask ? SRLoc(@"Command-") : @""),
-                                      (aModifierFlags & NSAlternateKeyMask ? SRLoc(@"Option-") : @""),
-                                      (aModifierFlags & NSControlKeyMask ? SRLoc(@"Control-") : @""),
-                                      (aModifierFlags & NSShiftKeyMask ? SRLoc(@"Shift-") : @""),
-                                      [[[SRKeyCodeTransformer sharedPlainASCIITransformer] transformedValue:@(aKeyCode)] uppercaseString]];
+            (aModifierFlags & NSCommandKeyMask ? SRLoc(@"Command-") : @""),
+            (aModifierFlags & NSAlternateKeyMask ? SRLoc(@"Option-") : @""),
+            (aModifierFlags & NSControlKeyMask ? SRLoc(@"Control-") : @""),
+            (aModifierFlags & NSShiftKeyMask ? SRLoc(@"Shift-") : @""),
+            c];
 }
