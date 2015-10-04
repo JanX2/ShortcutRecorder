3dcf6b63507b5d0c707b341d98872bbf38bada58
Ilya Kulakov <kulakov.ilya@gmail.com>
2015-10-04 03:48:12 -0400

    Define NSEdgeInsets and NSEdgeInsetsMake for pre-10.7 era.

--- Library/ShortcutRecorder.h
+++ Library/ShortcutRecorder.h
@@ -9,6 +9,7 @@
 //      Jesper
 //      Ilya Kulakov
 
+#import <Cocoa/Cocoa.h>
 #import <ShortcutRecorder/SRCommon.h>
 #import <ShortcutRecorder/SRKeyCodeTransformer.h>
 #import <ShortcutRecorder/SRModifierFlagsTransformer.h>
@@ -32,4 +33,22 @@
 
 #ifndef NSAppKitVersionNumber10_9
     #define NSAppKitVersionNumber10_9 1265
+#endif
+
+#ifndef NSEDGEINSETS_DEFINED
+    typedef struct NSEdgeInsets {
+        CGFloat top;
+        CGFloat left;
+        CGFloat bottom;
+        CGFloat right;
+    } NSEdgeInsets;
+
+    NS_INLINE NSEdgeInsets NSEdgeInsetsMake(CGFloat top, CGFloat left, CGFloat bottom, CGFloat right) {
+        NSEdgeInsets e;
+        e.top = top;
+        e.left = left;
+        e.bottom = bottom;
+        e.right = right;
+        return e;
+    }
 #endif
