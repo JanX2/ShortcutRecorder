2763e7839dcc2c909671e37041dc55b0b3dbb05d
Ilya Kulakov <kulakov.ilya@gmail.com>
2018-09-29 16:08:13 -0700

    Embed module maps into ShortcutRecorder and PTHotKey
    - Add an umbrella header for PTHotKey named HotKey.h
    - Do not explicitly link Demo app with ShortcutRecorder and PTHotKey

---
+++ PTHotKey/HotKey.h
@@ -0,0 +1,11 @@
+//
+//  HotKey.h
+//  PTHotKey.framework
+//
+
+
+#import <PTHotKey/PTHotKey.h>
+#import <PTHotKey/PTHotKeyCenter.h>
+#import <PTHotKey/PTKeyCodeTranslator.h>
+#import <PTHotKey/PTKeyCombo.h>
+#import <PTHotKey/PTHotKey+ShortcutRecorder.h>
